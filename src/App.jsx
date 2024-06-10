a016334d04a0eaa1483af8330e812ce6023c38b9
Eduarda Ramalho <eduardacns.ramalho@gmail.com>
2024-06-09 22:06:58 -0300

    feat: add user register route to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home';
+import HomePage from './pages/HomePage';
 import './App.css';
+import UserRegister from './pages/UserRegisterPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/funcionario" element={<UserRegister />} />
       </Routes>
     </BrowserRouter>
   );
